0c44fe49027edefa26d50861bf45a6fc43673ca5
Caleb James DeLisle <calebdelisle@lavabit.com>
2013-03-30 06:33:26 -0400

    Removed pom.xml and moved jquery-sheet-amd zip to a better location.

--- src/make.js
+++ src/make.js
@@ -8,21 +8,36 @@ var obj = new XWiki.model.JavaScriptExtension();
 obj.setCode(XWiki.Tools.contentFromFile("./src/js/tableedit.js"));
 doc.addXObject(obj);
 
-doc.addAttachment("src/main/resources/attachments/jquery-sheet-amd.zip");
+doc.addAttachment("src/attach/jquery-sheet-amd.zip");
 
-/*
-var BufferedWriter = require('buffered-writer');
-var stream = BufferedWriter.open("output.xml");
-xwiki.XMLWriter.write(doc.json, stream, function() {
-    stream.close();
-    console.log('done');
-});*/
 
-var pack = new XWiki.Package();
-pack.setName("XWiki - Contrib - XWiki.TableEdit");
-pack.setDescription("An editor which allows you to edit XWiki tables as spreadsheets using " +
-    "Jquery.sheet.");
-pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-tableedit");
-pack.addDocument(doc);
-pack.beginZip('XWiki.TableEdit.xar');
-pack.finalize();
+// Post to a wiki?
+// must post to a /preview/ page, for example:
+// syntax  ./do --post Admin:admin@192.168.1.1:8080/xwiki/bin/preview//
+var i = process.argv.indexOf('--post');
+if (i > -1) {
+    var userUrl = process.argv[i+1].split('@').reverse();
+    XWiki.Tools.postDocToWiki(userUrl[1], userUrl[0], doc, function() {
+        console.log("done");
+    });
+
+} else {
+
+    /*
+    var BufferedWriter = require('buffered-writer');
+    var stream = BufferedWriter.open("output.xml");
+    XWiki.XML.write(doc.json, stream, function() {
+        stream.close();
+        console.log('done');
+    });
+    */
+
+    var pack = new XWiki.Package();
+    pack.setName("XWiki - Contrib - XWiki.TableEdit");
+    pack.setDescription("An editor which allows you to edit XWiki tables as spreadsheets using " +
+        "Jquery.sheet.");
+    pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-tableedit");
+    pack.addDocument(doc);
+    pack.beginZip('XWiki.TableEdit.xar');
+    pack.finalize();
+}
