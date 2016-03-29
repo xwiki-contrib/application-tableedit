531f66316c39ea5fcaab91e6b7ce1600bd0e3135
Yann Flory <yann.flory@xwiki.com>
2016-03-29 18:11:28 +0200

    Update this.js to the new version (1.3-SNAPSHOT)

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -3,5 +3,5 @@ Package(function (pack) {
     pack.setDescription("An editor which allows you to edit XWiki tables as spreadsheets using " +
         "Jquery.sheet.");
     pack.setExtensionId("calebjamesdelisle:tableedit");
-    pack.setXarName("application-tableedit-1.2-SNAPSHOT.xar")
+    pack.setXarName("application-tableedit-1.3-SNAPSHOT.xar")
 });
